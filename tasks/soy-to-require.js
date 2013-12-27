b4855da79dfc83cdf61220edf04d8cbfad89f524
charliedowler <charliedowler15@gmail.com>
2013-12-27 14:52:21 +0000

    Use grunt task property

--- tasks/soy-to-require.js
+++ tasks/soy-to-require.js
@@ -1,20 +1,20 @@
 module.exports = function (grunt) {
   grunt.registerTask('soy-to-require', 'Append require wrapper', function () {
     var fs = require('fs');
-    var namespace = 'NAMESPACE';
-    var templatesDir = 'TEMPLATES'
+    var namespace = this.data.namespace
+    var templates = this.data.templates;
 
-    fs.readdir('templates', function (err, list) {
+    fs.readdir(templates, function (err, list) {
       list.forEach(function (filename, list) {
         var fileContents;
-        fs.readFile(__dirname + '\\' + templatesDir + '\\' + filename, function (err, data) {
+        fs.readFile(__dirname + '\\' + templates + '\\' + filename, function (err, data) {
           if (err) throw err;
           fileContents = data;
           var requireString = 'define(function(require) {' +
             '\nvar soy = require("soy");\n' + fileContents;
-          fs.writeFile(__dirname + '\\' + templatesDir + '\\' + filename, requireString, function (err) {
+          fs.writeFile(__dirname + '\\' + templates + '\\' + filename, requireString, function (err) {
             if (err) throw err;
-            fs.appendFile(__dirname + '\\' + templatesDir + '\\' + filename, '\nreturn ' + namespace + '.' + filename.split('.')[0] + '\n});', function (err) {
+            fs.appendFile(__dirname + '\\' + templates + '\\' + filename, '\nreturn ' + namespace + '.' + filename.split('.')[0] + '\n});', function (err) {
               if (err) throw err;
             });
           });
