d1bed60be69eefd6ce927be53250647a491edced
charliedowler <charliedowler15@gmail.com>
2014-02-01 17:20:23 +0000

    Example of input and output

---
+++ example/output/form.soy.js
@@ -0,0 +1,16 @@
+define(function (require) {
+  var soy = require("soy");
+// This file was automatically generated from form.soy.
+// Please don't edit this file by hand.
+
+  if (typeof MyNamespace == 'undefined') {
+    var MyNamespace = {};
+  }
+
+
+  MyNamespace.form = function (opt_data, opt_ignored) {
+    return '<form>Hello world</form>';
+  };
+
+  return MyNamespace.form;
+});
